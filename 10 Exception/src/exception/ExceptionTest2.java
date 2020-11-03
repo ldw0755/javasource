33ac0eb68dc5d76819ce104b54e06ca1260aa934
ldw0755 <65296543+ldw0755@users.noreply.github.com>
2020-11-03 15:40:58 +0900

    Update ExceptionTest2.java
    소스 수정

--- 10 Exception/src/exception/ExceptionTest2.java
+++ 10 Exception/src/exception/ExceptionTest2.java
@@ -8,6 +8,7 @@ public class ExceptionTest2 {
 		
 		int value1 = Integer.parseInt(data1);
 		int value2 = Integer.parseInt(data2);
+		System.out.println();
 		
 		System.out.println(value1 + value2);
 	}
